5651330d4d5b136c98c1e2acf28613d2b2a34642
Denny Wahyu <janexmgd@gmail.com>
2022-06-13 21:36:20 +0700

    intregasi edit profile seller

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import listUserReducer from "./listUser";
+import detailSellerReducer from "./detailSeller";
 
 export default combineReducers({
 	listUser: listUserReducer,
+	detailSeller: detailSellerReducer,
 });
