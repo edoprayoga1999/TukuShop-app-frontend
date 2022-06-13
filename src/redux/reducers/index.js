84f71c77153f151f603a833222c8194416ca4ca2
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-13 15:03:26 +0700

    Integrasi halaman home

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import listUserReducer from "./listUser";
+import listCategoryReducer from "./listCategory";
+import listNewProductReducer from "./listNewProduct";
+import listProductReducer from "./listProduct";
 
 export default combineReducers({
 	listUser: listUserReducer,
+	listCategory: listCategoryReducer,
+	listNewProduct: listNewProductReducer,
+	listProduct: listProductReducer,
 });
