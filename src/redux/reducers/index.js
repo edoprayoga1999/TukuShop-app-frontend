3e2ac92c29f371384fb57de904eadfa9599bc82e
edoprayoga1999 <edoprayoga.se@gmail.com>
2022-06-13 22:15:23 +0700

    remove strict mode and integrate cart page

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import listUserReducer from "./listUser";
 import listCategoryReducer from "./listCategory";
 import listNewProductReducer from "./listNewProduct";
 import listProductReducer from "./listProduct";
+import myCartReducers from "./myCart";
 
 export default combineReducers({
 	listUser: listUserReducer,
 	listCategory: listCategoryReducer,
 	listNewProduct: listNewProductReducer,
 	listProduct: listProductReducer,
+	myCart: myCartReducers
 });
