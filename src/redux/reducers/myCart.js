0083611466a7f2f7b41d5b81424981f128815793
edoprayoga1999 <edoprayoga.se@gmail.com>
2022-06-15 11:51:14 +0700

    fix cart

--- src/redux/reducers/myCart.js
+++ src/redux/reducers/myCart.js
@@ -14,20 +14,22 @@ const initialState = {
 const myCartReducers = (state = initialState, action) => {
 	switch (action.type) {
 	case GET_MY_CART_PENDING:
-		return { ...state, isLoading: true };
+		return { ...state, isLoading: true, isError: false, data: [], error: null };
 	case GET_MY_CART_SUCCESS:
 		return {
 			...state,
 			isLoading: false,
 			isError: false,
 			data: action.payload.data,
+			error: null
 		};
 	case GET_MY_CART_FAILED:
 		return {
 			...state,
 			isLoading: false,
 			isError: true,
-			error: action.payload,
+			data: [],
+			error: action.payload.message,
 		};
 	default:
 		return state;
