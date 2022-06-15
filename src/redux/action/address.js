8d507102dc0e0f8f92b95cb981ccebf63c28f0cf
edoprayoga1999 <edoprayoga.se@gmail.com>
2022-06-16 01:32:33 +0700

    fix checkout page

--- src/redux/action/address.js
+++ src/redux/action/address.js
@@ -17,3 +17,33 @@ export const addAddress = (form) => {
 			});
 	});
 };
+export const editAddress = (form, id) => {
+	return new Promise((resolve, reject) => {
+		axios.put(`${process.env.REACT_APP_API_URL}/address/${id}`, form, {
+			headers: {
+				token
+			}
+		})
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
+export const deleteAddress = (id) => {
+	return new Promise((resolve, reject) => {
+		axios.delete(`${process.env.REACT_APP_API_URL}/address/${id}`, {
+			headers: {
+				token
+			}
+		})
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
